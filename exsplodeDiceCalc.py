bb73e1ec9688ae82f547484e33f86b0eded827bd
andrewnemeth <andrewnemeth@optonline.net>
2019-05-15 15:14:41 -0400

    rounds, calcs chance of roll or higher

--- exsplodeDiceCalc.py
+++ exsplodeDiceCalc.py
@@ -2,8 +2,9 @@ from random import randint
 from collections import Counter
 import collections
 import csv
-sampleConut = 10000
+sampleConut = 1000000
 poolRange = (1,8)
+NUM_DECIMAL = 5
 
 def rollDie():
     hits = 0
@@ -21,12 +22,15 @@ def rollPool(num):
 
 def statsForPoolSice(poolsize):
     output = Counter([rollPool(poolsize) for x in range(sampleConut)])
+    del output[0]
     for key in output:
         output[key] = (output[key]*100)/sampleConut
 
     output = collections.OrderedDict(sorted(output.items()))
 
-    return [output[key] for key in output]
+    chancePerVal = [output[key] for key in output]
+    chanceAtLeastVal = [round(sum([chancePerVal[i] for i in range(j,len(chancePerVal))]),NUM_DECIMAL) for j in range (0, len(chancePerVal))]
+    return chanceAtLeastVal
 
 
 allData = [statsForPoolSice(x) for x in range (1,9)]
